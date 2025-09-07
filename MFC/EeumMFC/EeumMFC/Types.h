7ea368ca9d19d79f0752e66486369ef366a9e708
박주현 <yeochi1201@gmail.com>
2025-09-07 21:00:37 +0900

    feat:Analyzer Update
    Ah, WBGT, PMv/PPD, EWMA Update
    
    Jira : S13P21D208-189  S13P21D208-190 S13P21D208-192 S13P21D208-193

--- MFC/EeumMFC/EeumMFC/Types.h
+++ MFC/EeumMFC/EeumMFC/Types.h
@@ -12,8 +12,18 @@ struct IrEvent {
 };
 
 struct Metrics {
-	double tAvg{}, hAvg{};
-	double tEwma{}, hEwma{};
-	double dewPoint{}, heatIndex{};
-	bool spike{};
+    // 기존 값
+    double tAvg = NAN;     // 평균 온도
+    double hAvg = NAN;     // 평균 습도
+    double tEwma = NAN;    // EWMA 온도
+    double hEwma = NAN;    // EWMA 습도
+    double dewPoint = NAN; // 이슬점(°C)
+    double heatIndex = NAN;// 체감온도(°C)
+    bool   spike = false;
+
+    // 추가: 공조/쾌적 지표
+    double absHumidity = NAN; // 절대습도 (g/m^3)
+    double wbgt = NAN;        // WBGT (근사, °C)
+    double pmv = NAN;         // PMV (-3 ~ +3)
+    double ppd = NAN;         // PPD (%)
 };
