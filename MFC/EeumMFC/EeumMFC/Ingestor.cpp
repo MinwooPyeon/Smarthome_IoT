542e582779c0450884cfcd133edb87a5b3423f4a
박주현 <yeochi1201@gmail.com>
2025-09-07 21:40:07 +0900

    feat:Ingestor Implement
    Jira : S13P21D208-207

---
+++ MFC/EeumMFC/EeumMFC/Ingestor.cpp
@@ -0,0 +1,32 @@
+#include "pch.h"
+#include "Ingestor.h"
+
+void Ingestor::setCallback(MetricsCallback callback) {
+	onTick_ = std::move(callback);
+}
+
+void Ingestor::start(double hz) {
+	timer_.start(hz, [this] {this->tickOnce(); });
+}
+
+void Ingestor::stop() {
+	timer_.stop();
+}
+
+void Ingestor::pushEnv(const EnvSample& env) {
+	envBuf_.push(env);
+}
+
+void Ingestor::pushIr(const IrEvent& ir) {
+	irBuf_.push(ir);
+}
+
+void Ingestor::tickOnce() {
+	auto env = envBuf_.flush();
+	auto ir = irBuf_.flush();
+
+	if (env.empty() && ir.empty()) return;
+
+	auto met = analyzer_.compute(env);
+	if (onTick_) onTick_(env, ir, met);
+}
