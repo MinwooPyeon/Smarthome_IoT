9b5404cec4df94f5b38ceff01036350903557672
박주현 <yeochi1201@gmail.com>
2025-09-09 11:49:34 +0900

    fix:LNK2019 - Ingetstor.obj
    Cause : MsgBuffer Implement - Template Class need to be implement at
    header file
    
    Jira : S13P21D208-183

--- MFC/EeumMFC/EeumMFC/MsgBuffer.h
+++ MFC/EeumMFC/EeumMFC/MsgBuffer.h
@@ -5,13 +5,24 @@
 template<typename T>
 class MsgBuffer {
 public:
-	void push(const T& msg);
+	void push(const T& msg) {
+		std::lock_guard<std::mutex> lock(mtx);
+		buffer.push_back(msg);
+	}
 
-	std::vector<T> flush();
+	std::vector<T> flush() {
+		return std::vector<T>();
+	}
 
-	size_t size() const;
+	size_t size() const {
+		std::lock_guard<std::mutex> lock(mtx);
+		return buffer.size();
+	}
 
-	void clear();
+	void clear() {
+		std::lock_guard<std::mutex> lock(mtx);
+		buffer.clear();
+	}
 
 private:
 	std::vector<T> buffer;
