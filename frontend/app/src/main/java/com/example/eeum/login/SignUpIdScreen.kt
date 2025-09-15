c480065af565b7021c532713f14c27f6e6e8c6cd
이채영 <0929alice@naver.com>
2025-09-15 15:11:54 +0900

    feat: 로그인 화면, 액티비티 생성
    로그인 화면, 액티비티 생성
    
    Resolves: S13P21D208-42

---
+++ frontend/app/src/main/java/com/example/eeum/login/SignUpIdScreen.kt
@@ -0,0 +1,18 @@
+package com.example.eeum.login
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.eeum.ui.theme.EeumTheme
+
+@Composable
+fun SignUpIdScreen() {
+
+}
+
+@Preview
+@Composable
+private fun SignUpIdScreenPreview() {
+    EeumTheme(dynamicColor = false) {
+        SignUpIdScreen()
+    }
+}
