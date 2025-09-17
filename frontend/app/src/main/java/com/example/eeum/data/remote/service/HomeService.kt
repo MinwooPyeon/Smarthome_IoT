1b7a254694f763801e66a4abaf2cececb5085480
이채영 <0929alice@naver.com>
2025-09-17 14:38:10 +0900

    feat: 대표평면도 변경 api 연결을 위한 밑작업
    Resolves: S13P21D208-43

--- frontend/app/src/main/java/com/example/eeum/data/remote/service/HomeService.kt
+++ frontend/app/src/main/java/com/example/eeum/data/remote/service/HomeService.kt
@@ -1,12 +1,20 @@
 package com.example.eeum.data.remote.service
 
 import com.example.eeum.data.model.response.home.AllUserHome
+import com.example.eeum.data.model.response.home.PrimaryHome
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.PUT
+import retrofit2.http.Path
 
 interface HomeService {
 
     //유저 집 목록 조회
     @GET("/api/user-home/address")
     suspend fun getUserHomes(): Response<AllUserHome>
+
+    @PUT("/api/user-home/{homeId}/primary")
+    suspend fun setPrimaryHome(
+        @Path("homeId") homeId: Int
+    ): Response<PrimaryHome>
 }
