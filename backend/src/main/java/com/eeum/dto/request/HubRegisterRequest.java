0eca83577bda23e300f2b73b0df6d98da2d7890e
이유민 <leeum1111@naver.com>
2025-09-18 14:22:39 +0900

    fix: hub inet swagger 오류 수정

--- backend/src/main/java/com/eeum/dto/request/HubRegisterRequest.java
+++ backend/src/main/java/com/eeum/dto/request/HubRegisterRequest.java
@@ -1,5 +1,6 @@
 package com.eeum.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class HubRegisterRequest {
+    @Schema(description = "허브 IP 주소 (PostgreSQL inet)", example = "192.168.0.101", format = "ipv4")
 	private String deviceAddr;
     private Integer homeId;
 }
